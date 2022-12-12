8a59e96f865ffb58ce0df18623b14237109217cb
Pedro Flores Crespo <55706855+PedroMFC@users.noreply.github.com>
2022-12-12 19:57:11 +0000

    2022: day 12 solutions

--- 2022/day12/day12.py
+++ 2022/day12/day12.py
@@ -1,8 +1,8 @@
 class Node:
-    def __init__(self, position: tuple[int, int], real_cost: int = 0):
+    def __init__(self, position: tuple[int, int]):
         self.postion = position
-        self.real_cost = real_cost
-        self.heuristic_cost = real_cost
+        self.real_cost = 0
+        self.heuristic_cost = 0
         self.parent = None
 
     def set_parent(self, parent) -> None:
@@ -59,14 +59,15 @@ def read_input(file_name: str) -> list[list[str]]:
 
     return area
 
-def calculate_position(area: list[list[str]], character: str) -> list[int]:
+def calculate_position(area: list[list[str]], character: str) -> list[list[int]]:
+    postions = []
 
     for i, row in enumerate(area):
         for j, value in enumerate(row):
             if value == character:
-                return [i,j]
+                postions.append([i,j])
 
-    return [None, None]
+    return postions
 
 
 def can_be_reached(origen: str, destino: str) -> bool:
@@ -83,10 +84,8 @@ def valid_position(postion: tuple[int, int], num_rows: int, num_columns: int) ->
 '''
 https://medium.com/@nicholas.w.swift/easy-a-star-pathfinding-7e6689c7f7b2
 '''
-def calculate_path(area: list[list[str]]) -> int:
-    start = calculate_position(area, 'S')
-    end = calculate_position(area, 'E')
 
+def calculate_path(area: list[list[str]], start: tuple[int, int], end: tuple[int, int]) -> list[Node]:
     num_rows = len(area)
     num_columns = len(area[0])
 
@@ -112,7 +111,7 @@ def calculate_path(area: list[list[str]]) -> int:
             return path
 
         children = []
-        for new_position in [(0,1), (1, 0), (0, -1), (-1,0) ]:
+        for new_position in [ (0,1), (1, 0), (0, -1), (-1,0) ]:
             node_position = (actual_position[0] - new_position[0], actual_position[1] - new_position[1])
 
             if ( not valid_position(node_position, num_rows, num_columns) or
@@ -146,15 +145,77 @@ def calculate_path(area: list[list[str]]) -> int:
                 open_list.append(child)
             
 
-        open_list.sort()      
+        open_list.sort()
 
+def calculate_reverse_path(area: list[list[str]], start: tuple[int, int], search_str: str) -> list[Node]:
+    num_rows = len(area)
+    num_columns = len(area[0])
+
+    open_list = [Node((start[0], start[1]))]
+    close_list = []
+
+    while len(open_list) > 0:
+        actual_node = open_list[0]
+        actual_position = actual_node.get_position()
+
+        open_list.pop(0)
+        close_list.append(actual_node)
+
+        if area[actual_position[0]][actual_position[1]] == search_str:
+            path = []
+            actual = actual_node
+
+            while actual is not None:
+                path.append(actual)
+                actual = actual.get_parent()
+            return path
+
+        children = []
+        for new_position in [ (0,1), (1, 0), (0, -1), (-1,0) ]:
+            node_position = (actual_position[0] - new_position[0], actual_position[1] - new_position[1])
+
+            if ( not valid_position(node_position, num_rows, num_columns) or
+                not can_be_reached(area[node_position[0]][node_position[1]], area[actual_position[0]][actual_position[1]])):
+                continue
+
+            child = Node(node_position)
+            child.set_parent(actual_node)
+            children.append(child)
+
+
+        for child in children:
+
+            child_is_closed = list(filter(lambda node_child: child == node_child, close_list))
+            if len(child_is_closed) > 0:
+                continue
+
+            child.set_real_cost(child.get_parent().get_real_cost() + 1)
+
+            insert_child = True
+            for index, open_child in enumerate(open_list):
+                if child == open_child:
+                    if child.get_real_cost() < open_child.get_real_cost():
+                        open_list.pop(index)
+                    else:
+                        insert_child = False
+                    break
+
+            if insert_child:
+                open_list.append(child)
+            
+
+        open_list.sort()
 
 def main() -> None:
     area = read_input('2022/day12/input.txt')
 
-    path = calculate_path(area)
+    path = calculate_path(area, calculate_position(area, 'S')[0], calculate_position(area, 'E')[0])
     
-    print(f'The fewest step required are {len(path)-1}')
+    print(f'The fewest step from S required are {len(path)-1}')
+
+    a_path = calculate_reverse_path(area, calculate_position(area, 'E')[0], 'a')
+
+    print(f'The fewest step from any a required are {len(a_path)-1}')
 
 if __name__ == "__main__":
     main()
